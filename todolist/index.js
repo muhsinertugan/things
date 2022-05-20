f8b97e80090998e900ac4c34b36fc9ec2f30364a
muhsin.ertugan <muhsin.ertugan@gmail.com>
2022-05-20 19:33:07 +0300

    list item and select added

--- todolist/index.js
+++ todolist/index.js
@@ -1,1 +1,20 @@
+const ulDom = document.querySelector("#list");
+const thingsToDo = document.querySelector("#task");
 
+const newElement = () => {
+  const liDom = document.createElement("li");
+  liDom.addEventListener("click", (e) =>
+    liDom.className == ""
+      ? e.currentTarget.classList.add("checked")
+      : e.currentTarget.classList.remove("checked")
+  );
+
+  if (thingsToDo.value == "") {
+    $(".error").toast("show");
+  } else {
+    liDom.innerHTML = thingsToDo.value;
+    ulDom.appendChild(liDom);
+    $(".success").toast("show");
+  }
+  thingsToDo.value = "";
+};
